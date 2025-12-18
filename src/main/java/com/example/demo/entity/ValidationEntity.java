420466194e32ef7dff404fa40c2cd0b091740adc
bala-amypo <bala@amypo.in>
2025-12-18 08:27:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -20,8 +20,7 @@ public class ValidationEntity{
     @NotNull(message="Password is mandatory")
     private String password;
     @Max(20)
-    @
-
+    @Positive(message="Age must be a Positive number")
     private Integer age;
       public ValidationEntity(Long id,String username,String email,String password){
         this.id=id;
@@ -29,4 +28,31 @@ public class ValidationEntity{
         this.email=email;
         this.password=password;
     }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public Long getId(){
+        return id;
+    }
+    public ValidationEntity()[
+        
+    ]
 }
